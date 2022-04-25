674459e037077ca087232295228ca54d7bf710e6
Tobi Balogun <tobi.balo@gmail.com>
2022-04-25 22:57:34 +0100

    complete function "Binary to decimal"

---
+++ 2022-04-25/01/app.js
@@ -0,0 +1,14 @@
+// Bin to decimal
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// convert binary to string
+// split, convert to number then reverse
+// multiply element of array by 2^index 0, add to next element multiplied by 2^index 1 ... by 2^index n
+function binToDec(bin) {
+    return bin
+        .toString()
+        .split("")
+        .map(Number)
+        .reverse()
+        .reduce((acc, element, index) => acc + element * 2 ** index, 0);
+}
