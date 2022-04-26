07f1733c61c1c04e0f699206ab0e8084eabbf6f4
Tobi Balogun <tobi.balo@gmail.com>
2022-04-26 08:42:08 +0100

    String cleaning

---
+++ 2022-04-26/01/app.js
@@ -0,0 +1,21 @@
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+// Take in the string and split it to array (for easy manipulation)
+// Convert all spaces to a custom delimiter
+// Delete all numbers
+// Convert array to string using custom delimiter
+
+function stringClean(s) {
+    return s
+        .split("")
+        .map((x) => (x === " " ? "customdelim" : x))
+        .map((x, i) => (isNaN(x) ? x : s.slice(i, i)))
+        .map((x) => (x === "customdelim" ? " " : x))
+        .join("");
+}
