cbb48dba34298fa8a7fda72cebe7ae5b994eafb5
Tobi Balogun <tobi.balo@gmail.com>
2022-04-27 09:32:20 +0100

    Complete "Cat years, Dog years"

---
+++ 2022-04-27/01/app.js
@@ -0,0 +1,17 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+    let catYears = 0,
+        dogYears = 0;
+    for (let i = 1; i <= humanYears; i++) {
+        if (i === 1) {
+            catYears += 15;
+            dogYears += 15;
+        } else if (i === 2) {
+            catYears += 9;
+            dogYears += 9;
+        } else {
+            catYears += 4;
+            dogYears += 5;
+        }
+    }
+    return [humanYears, catYears, dogYears];
+};
