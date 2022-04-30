bd2a008accd482a038b5d66e1aa80268596567f0
Tobi Balogun <tobi.balo@gmail.com>
2022-04-30 18:14:49 +0100

    add *new discovery

--- 2022-04-30/02/app.js
+++ 2022-04-30/02/app.js
@@ -8,7 +8,7 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 function countPositivesSumNegatives(input) {
-    if (input === null || input.length === 0) return [];
+    if (input === null || input.length === 0) return []; // comparing with "null" as the second condition causes the test to fail
     let countPositiveNums = input.filter((x) => x > 0).length;
     let sumNegativeNums = input.filter((x) => x < 0).reduce((acc, x) => (acc += x), 0);
     return [countPositiveNums, sumNegativeNums];
