8b6fc57c16ac8f67042cd189912ee22e67da36bb
Tobi Balogun <tobi.balo@gmail.com>
2022-05-06 04:43:45 +0100

    Complete "Sum Mixed Array" kata

---
+++ 2022-05-06/01/app.js
@@ -0,0 +1,7 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x) {
+    return x.reduce((acc, e) => (acc += +e), 0);
+}
