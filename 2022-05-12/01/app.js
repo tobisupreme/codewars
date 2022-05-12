a4cc600f1d26b71900e1b79cb55d565dc1abf137
Tobi Balogun <tobi.balo@gmail.com>
2022-05-12 21:55:19 +0100

    Complete "Convert a Boolean to a String" kata

---
+++ 2022-05-12/01/app.js
@@ -0,0 +1,15 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b) {
+    if (b === true) {
+        return "true";
+    } else if (b === false) {
+        return "false";
+    } else {
+        return "What you give me?";
+    }
+}
