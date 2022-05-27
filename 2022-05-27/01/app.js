d7343a67d1d0f66c495a09608f61d78f1fd59185
Tobi Balogun <tobi.balo@gmail.com>
2022-05-27 05:42:43 +0100

    Add kata completed on May 27

---
+++ 2022-05-27/01/app.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+// You Can't Code Under Pressure #1
+
+// i will be an integer. Double it and return it.
+function doubleInteger(i) {
+  if (isNan(i)) {
+    return;
+  }
+  return i * 2;
+}
