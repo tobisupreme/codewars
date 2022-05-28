e91e42ef82dd3c577ce6f48bc2d0f5a3fa6ec0ee
Tobi Balogun <tobi.balo@gmail.com>
2022-05-28 05:55:05 +0100

    Refactor "Negative numbers" kata

--- 2022-05-28/01/app.js
+++ 2022-05-28/01/app.js
@@ -1,9 +1,15 @@
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
+// function makeNegative(num) {
+//   if (num > 0) {
+//     return num * -1;
+//   }
+//   return num;
+// }
+
 function makeNegative(num) {
-  if (num > 0) {
-    return num * -1;
+    (num > 0) ? num *= -1 : num;
+    return num
   }
-  return num;
-}
+  
