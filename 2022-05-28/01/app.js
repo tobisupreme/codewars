277ef5cfe7592fc6ae541c6229e0949399e49a7c
Tobi Balogun <tobi.balo@gmail.com>
2022-05-28 05:54:15 +0100

    Complete "Negative numbers" kata

---
+++ 2022-05-28/01/app.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+  if (num > 0) {
+    return num * -1;
+  }
+  return num;
+}
