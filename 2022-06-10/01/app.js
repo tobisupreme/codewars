a03aa3930a9d0cbff80c8f5b68cf201d06008da4
Tobi Balogun <tobi.balo@gmail.com>
2022-06-13 08:13:44 +0100

    Complete "Find Maximum and Minimum Values of a List" kata

--- 2022-06-10/01/app.js
+++ 2022-06-10/01/app.js
@@ -1,14 +1,14 @@
-var min = function(list){
-  return Math.min.apply(null, list)
-}
+var min = function (list) {
+  return Math.min.apply(null, list);
+};
 
-var max = function(list){
-  return Math.max.apply(null, list)
-}
+var max = function (list) {
+  return Math.max.apply(null, list);
+};
 
 // Test cases
-console.log(min([4,6,2,1,9,63,-134,566]))
-console.log(max([4,6,2,1,9,63,-134,566]))
+console.log(min([4, 6, 2, 1, 9, 63, -134, 566]));
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
 
-console.log(min([-52, 56, 30, 29, -54, 0, -110]))
-console.log(max([-52, 56, 30, 29, -54, 0, -110]))
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
