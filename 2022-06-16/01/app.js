b97a0c83e50f85a8c9590721324731c04b317810
Tobi Balogun <tobi.balo@gmail.com>
2022-06-16 06:15:51 +0100

    Complete "Vowel Count" kata

--- 2022-06-16/01/app.js
+++ 2022-06-16/01/app.js
@@ -1,7 +1,17 @@
 function getCount(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
   let vowelsCount = 0;
 
-  // enter your magic here
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.indexOf(str[i]) != -1) {
+      vowelsCount++
+    }
+  }
 
   return vowelsCount;
 }
+
+
+// Test cases
+let test1 = getCount('abracadabra');
+console.log(test1);
