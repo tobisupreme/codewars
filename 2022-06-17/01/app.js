3ee7a14680ff2e6e1372c501b0eb7dcb08215ce5
Tobi Balogun <tobi.balo@gmail.com>
2022-06-17 19:22:42 +0100

    Attempted memoization as a solution but can't handle big numbers

--- 2022-06-17/01/app.js
+++ 2022-06-17/01/app.js
@@ -1,9 +1,24 @@
-function factorial(n){
-  // Add some code
+let factorial_cache = {};
+
+function factorial(n) {
+  let value;
+  if (n in factorial_cache) {
+    return factorial_cache[n];
+  } else if (n < 1) {
+    value = null;
+  } else if (n == 1) {
+    value = 1;
+  } else if (n == 2) {
+    value = 2;
+  } else {
+    value = n * factorial(n - 1);
+    factorial_cache[n] = value;
+  }
+  return value.toString();
 }
 
 // Test cases
-console.log(factorial(1)) // '1'
-console.log(factorial(5)) // '120'
-console.log(factorial(9)) // '362880'
-console.log(factorial(15)) // '1307674368000'
+console.log(factorial(1)); // '1'
+console.log(factorial(5)); // '120'
+console.log(factorial(9)); // '362880'
+console.log(factorial(15)); // '1307674368000'
