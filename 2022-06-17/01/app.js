9521fdeb7678b77d675dcfe118577c7d07dd9315
Tobi Balogun <tobi.balo@gmail.com>
2022-06-17 18:58:29 +0100

    Initial commit - "Large Factorials" kata

---
+++ 2022-06-17/01/app.js
@@ -0,0 +1,9 @@
+function factorial(n){
+  // Add some code
+}
+
+// Test cases
+console.log(factorial(1)) // '1'
+console.log(factorial(5)) // '120'
+console.log(factorial(9)) // '362880'
+console.log(factorial(15)) // '1307674368000'
