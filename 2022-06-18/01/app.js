6827c8abe532b21661590f556e06529ac736e771
Tobi Balogun <tobi.balo@gmail.com>
2022-06-18 01:26:12 +0100

    Add solution to "Repeat Strings" kata

--- 2022-06-18/01/app.js
+++ 2022-06-18/01/app.js
@@ -6,7 +6,15 @@ function repeatStr(n, s) {
   return result;
 }
 
+function repeatStr2(n, s) {
+  return s.repeat(n);
+}
+
 // Test cases
 console.log(repeatStr(3, "*"));
 console.log(repeatStr(5, "#"));
 console.log(repeatStr(2, "ha "));
+
+console.log(repeatStr2(3, "*"));
+console.log(repeatStr2(5, "#"));
+console.log(repeatStr2(2, "ha "));
