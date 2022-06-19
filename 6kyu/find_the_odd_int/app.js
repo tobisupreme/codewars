37d834a9411cb8747c2ef18fb949ee6201267d24
Tobi Balogun <tobi.balo@gmail.com>
2022-06-19 08:32:51 +0100

    Initial commit - "Find the odd int" kata

---
+++ 6kyu/find_the_odd_int/app.js
@@ -0,0 +1,12 @@
+function findOdd(A) {
+  
+  return 0;
+}
+
+// Test cases
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])) // 5
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])) // -1
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])) // 5
+console.log(findOdd([10])) // 10
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1])) // 10
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10])) // 1
