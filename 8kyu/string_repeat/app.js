09fe15b1e67184a679328a07545d46a1d5ebaa7f
Tobi Balogun <tobi.balo@gmail.com>
2022-06-19 09:59:00 +0100

    Add PREP to "String Repeat" kata

--- 8kyu/string_repeat/app.js
+++ 8kyu/string_repeat/app.js
@@ -1,3 +1,20 @@
+/*
+PREP:
+Parameters:
+This function takes in two parameters: a string, s and an integer, n
+
+Returns:
+This function returns the given string n times
+
+Examples:
+input of 6 & "I"     should return "IIIIII"
+input of 5 & "Hello" should return "HelloHelloHelloHelloHello"
+
+Pseudocode:
+// Function takes in a string and number
+// Return string number times
+*/
+
 function repeatStr(n, s) {
   let result = "";
   for (i = 0; i < n; i++) {
