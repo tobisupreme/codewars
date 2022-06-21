d8f249df4cda5f8b169443790e8b732f6b2e9798
Tobi Balogun <tobi.balo@gmail.com>
2022-06-21 21:17:52 +0100

    Add PREP to "SevenAte9" kata

--- 7kyu/SevenAte9/app.js
+++ 7kyu/SevenAte9/app.js
@@ -1,3 +1,26 @@
+/*
+PREP:
+Parameters:
+This function takes in a string
+
+Returns:
+This function returns a string
+
+Example:
+input of '797' returns '77'
+input of '7979797' returns '7777'
+input of '165561786121789797' returns '16556178612178977'
+
+Pseudocode:
+// Function takes in a string
+  // Split string into array for mutability
+  // Iterate through elements of array
+    // If current element is 9 and elements to the left and right are 7
+      // remove current element, start loop from begining
+  // Convert array to string
+  // Return string
+*/
+
 function sevenAte9(str) {
   
 }
