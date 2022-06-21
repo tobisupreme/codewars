584e4843a941d31871ce1e0207657b0bb0a97c3b
Tobi Balogun <tobi.balo@gmail.com>
2022-06-21 21:46:51 +0100

    Add test cases to "Capitalizationa and Mutability" kata

--- 8kyu/capitalization_and_mutability /app.js
+++ 8kyu/capitalization_and_mutability /app.js
@@ -4,6 +4,27 @@
 
 // Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
 
+/*
+Parameters: 
+This function takes in a string
+
+Returns:
+This function returns a string
+
+Examples:
+input of 'word' returns 'Word'
+input of 'i' returns 'I'
+input of 'glasswear' returns 'Glasswear'
+
+Pseudocode:
+Capitalise first letter of word
+Add substring of other letters (excluding the first word)
+*/
+
 function capitalizeWord(word) {
-    return word[0].toUpperCase() + word.slice(1);
+  return word[0].toUpperCase() + word.slice(1);
 }
+
+console.log(capitalizeWord("word"));
+console.log(capitalizeWord("i"));
+console.log(capitalizeWord("glasswear"));
