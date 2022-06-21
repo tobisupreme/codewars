005278fb95a0186a4c729ff6ab5d0844aba0db4d
Tobi Balogun <tobi.balo@gmail.com>
2022-06-21 21:59:22 +0100

    Add PREP to "Count of positives / sum of negatives" kata

--- 8kyu/count_of_positives_/_sum_of_negatives /app.js
+++ 8kyu/count_of_positives_/_sum_of_negatives /app.js
@@ -4,8 +4,23 @@
 
 // If the input is an empty array or is null, return an empty array.
 
-// Example
+/* PREP:
+Parameters:
+This function takes in an array of integers
+
+Returns:
+This function returns an array of integers
+
+Example:
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+Pseudocode:
+// Declare variables to sumPositiveNumbers and sumNegativeNumbers
+// Iterate through the array
+    // If element is greater than zero, add to sumPositiveNumbers
+    // If element is less than zero, add to sumNegativeNumbers
+// Return array [sumPositiveNumbers, sumNegativeNumbers]
+*/
 
 function countPositivesSumNegatives(input) {
     if (input === null || input.length === 0) return []; // comparing with "null" as the second condition causes the test to fail
---
+++ 8kyu/count_of_positives_/_sum_of_negatives /readme.md
@@ -0,0 +1,15 @@
+View [this kata](https://www.codewars.com/kata/576bb71bbbcf0951d5000044) on [Codewars](https://www.codewars.com)
+
+# Name Shuffler
+
+## Instructions
+
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+## Example
+
+For input `[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]`, you should return `[10, -65]`.
