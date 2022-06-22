44f040a7e19697a28bdcbfc7d059bb91eda6e9b3
Tobi Balogun <tobi.balo@gmail.com>
2022-06-22 06:58:53 +0100

    Add PREP to "String Clean" kata

--- 8kyu/string_clean/app.js
+++ 8kyu/string_clean/app.js
@@ -1,15 +1,24 @@
 // Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
 
-// Examples (input -> output)
-// '! !'                 -> '! !'
-// '123456789'           -> ''
-// 'This looks5 grea8t!' -> 'This looks great!'
-// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+/* 
+// PREP:
+// Paramaeters:
+    This function will take in a string
 
-// Take in the string and split it to array (for easy manipulation)
-// Convert all spaces to a custom delimiter
-// Delete all numbers
-// Convert array to string using custom delimiter
+// Returns:
+    This function cleans out all numeric characters, and returns a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+// Example:
+    input of '! !'                 returns '! !'
+    input of '123456789'           returns ''
+    input of 'This looks5 grea8t!' returns 'This looks great!'
+
+// Pseudocode:
+    // Take in the string and split it to array (for easy manipulation)
+    // Convert all spaces to a custom delimiter
+    // Delete all numbers
+    // Convert array to string using custom delimiter
+*/
 
 function stringClean(s) {
     return s
