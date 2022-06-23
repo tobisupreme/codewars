cc3dbc9be1c780f2bbf6ccaee152ec4cce175599
Tobi Balogun <tobi.balo@gmail.com>
2022-06-23 14:20:27 +0100

    Add test cases to "Hex to Decimal" kata

--- 8kyu/hex_to_decimal/how_many_stairs_will_suzuki_climb_in_20_years?/app.js
+++ 8kyu/hex_to_decimal/how_many_stairs_will_suzuki_climb_in_20_years?/app.js
@@ -1,3 +1,10 @@
 function hexToDec(hexString){
   //your code here
 }
+
+// Test cases
+let test1 = hexToDec("1") // 1
+let test2 = hexToDec("a") // 10
+let test3 = hexToDec("10") // 16
+let test4 = hexToDec("FF") // 255
+let test5 = hexToDec("-C") // -12
