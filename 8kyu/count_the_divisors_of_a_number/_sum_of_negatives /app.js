dd980b5def6f7355463ee470ff26262157b0214f
Tobi Balogun <tobi.balo@gmail.com>
2022-06-24 06:43:32 +0100

    Add PREP to "Count the divisors of a number" kata

--- 8kyu/count_the_divisors_of_a_number/_sum_of_negatives /app.js
+++ 8kyu/count_the_divisors_of_a_number/_sum_of_negatives /app.js
@@ -1,3 +1,23 @@
+/* PREP:
+Parameters:
+This function takes in a number
+
+Returns:
+This function returns a number
+
+Example:
+input of 4 returns 3 (1, 2, 4)
+input of 5 returns 2 (1, 5)
+input of 12 returns 6 (1, 2, 3, 4, 6, 12)
+input of 30 returns 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+Pseudocode:
+// Declare variable count
+// From i = 1 to i = n
+    // If n % i == 0, increase count
+// Return count
+*/
+
 function getDivisorsCnt(n) {
   // todo
 }
