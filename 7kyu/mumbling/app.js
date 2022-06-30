e49481bb463fd5addc7e26acdbcaa4ea6f0edbbb
Tobi Balogun <tobi.balo@gmail.com>
2022-06-30 05:17:30 +0100

    Add PREP to "Mumbling" kata

--- 7kyu/mumbling/app.js
+++ 7kyu/mumbling/app.js
@@ -1,3 +1,28 @@
+/*
+PREP:
+Parameters:
+This function takes in a string which includes only letters from a..z and A..Z
+
+Returns:
+This function returns a string
+
+Example:
+input of "ZpglnRxqenU" returns "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+input of "NyffsGeyylB" returns "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+input of "MjtkuBovqrU" returns "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+input of "EvidjUnokmM" returns "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+input of "HbideVbxncC" returns "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+
+Pseudocode:
+// Function takes in a string
+  // Split string into array for mutability
+  // Iterate through elements of array
+    // Replace each element in array with element (index + 1) times
+    // Make first instance uppercase
+  // Convert array to string with "-" delimiter
+  // Return string
+*/
+
 function accum(s) {
 	// your code
 }
