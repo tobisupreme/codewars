5d7e7f440f30a4b36edf029031655ece1481ba14
Tobi Balogun <tobi.balo@gmail.com>
2022-07-01 20:05:04 +0100

    Add PREP to "Find the first non-consecutive number" kata

--- 8kyu/2022-04-28/find_the_first_non-consecutive_number/app.js
+++ 8kyu/2022-04-28/find_the_first_non-consecutive_number/app.js
@@ -1,3 +1,23 @@
+/*
+PREP:
+Parameters:
+This function takes in an array
+
+Returns:
+The function returns a number
+
+Examples:
+Input of [1,2,3,4,6,7,8] returns 6
+
+Pseudocode:
+* if the array has one element or the array is empty, return null
+* declare a variable j and set it to the first element of the array, arr[0]
+* Loop through the array starting with the second element, arr[1]
+    ** if the difference between the current element and the declared variable is not 1, return the current element
+    ** else, assign the value of current element to variable j
+* return null 
+*/
+
 function firstNonConsecutive(arr) {
     if (arr.length === 1 || arr === []) {
         return null;
