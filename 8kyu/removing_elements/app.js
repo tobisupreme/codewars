7efa16e2f6691836fd052c875a0695b95e7926c0
Tobi Balogun <tobi.balo@gmail.com>
2022-07-04 20:25:17 +0100

    Add test cases to "Removing Elements" kata

--- 8kyu/removing_elements/app.js
+++ 8kyu/removing_elements/app.js
@@ -22,3 +22,9 @@ function removeEveryOther(arr) {
   }
   return arr;
 }
+
+// Test cases
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"])) // ["Hello", "Hello Again"]);
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // [1, 3, 5, 7, 9]);
+console.log(removeEveryOther([[1, 2], [1, 2]])) // [[1, 2]]);
+console.log(removeEveryOther([["Goodbye"], { Great: "Job" }])) // [["Goodbye"]]);
