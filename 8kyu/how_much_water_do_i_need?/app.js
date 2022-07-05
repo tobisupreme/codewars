6bb8e77765c91607623fa01ef88f2dd969f0fdd5
Tobi Balogun <tobi.balo@gmail.com>
2022-07-05 20:52:41 +0100

    Add test cases to "How much water do I need?" kata

--- 8kyu/how_much_water_do_i_need?/app.js
+++ 8kyu/how_much_water_do_i_need?/app.js
@@ -24,3 +24,9 @@ function howMuchWater(water, load, clothes) {
     return Number((water * 1.1 ** (clothes - load)).toFixed(2));
   }
 }
+
+// Test cases
+console.log(howMuchWater(10,10,21)) // 'Too much clothes'
+console.log(howMuchWater(10,10,2)) // 'Not enough clothes'
+console.log(howMuchWater(10,11,20)) // 23.58
+console.log(howMuchWater(50,15,29)) // 189.87
