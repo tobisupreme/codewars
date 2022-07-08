abc6cfe6a36befcf6b7f75852e52a323260d43f9
Tobi Balogun <tobi.balo@gmail.com>
2022-07-08 08:44:51 +0100

    initial commit - "Square sum" kata

---
+++ 8kyu/square_sum/app.js
@@ -0,0 +1,12 @@
+function squareSum(numbers) {
+
+}
+
+// Test cases
+const test1 = squareSum([1, 2]) // 5
+const test2 = squareSum([0, 3, 4, 5]) // 50
+const test3 = squareSum([]) // 0
+
+console.log(test1)
+console.log(test2)
+console.log(test3)
