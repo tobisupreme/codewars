055c4382ae81caf760d159bca813dca41e4b69e8
Tobi Balogun <tobi.balo@gmail.com>
2022-07-12 04:55:53 +0100

    Add PREP to "Reversed Sequence" kata

--- 8kyu/reversed_sequence/app.js
+++ 8kyu/reversed_sequence/app.js
@@ -1,8 +1,26 @@
+/* 
+PREP:
+Parameters:
+This function takes in a number
+
+Returns:
+This function returns an array
+
+Examples:
+See `readme.md`
+
+Pseudocode:
+// Function takes in a number
+// Create an empty array, array
+// While number > 0, add number to array. n = n - 1
+// return array
+*/
+
 const reverseSeq = n => {
   return [];
 };
 
-// Test cases
+// Test case
 const test1 = reverseSeq(5) // [5, 4, 3, 2, 1]
 
 console.log(test1);
