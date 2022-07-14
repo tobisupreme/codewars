51153509b4f958ea6bf37acbe3dc47cd04c77b8d
Tobi Balogun <tobi.balo@gmail.com>
2022-07-14 05:07:21 +0100

    Add PREP to "Convert a Number to a String!" kata

---
+++ 8kyu/convert_a_number_to_a_string!/app.js
@@ -0,0 +1,25 @@
+/* 
+PREP:
+Parameters:
+This function takes in a number
+
+Returns:
+This function returns a string
+
+Examples:
+See `readme.md`
+
+Pseudocode:
+// Function takes in a number
+// Convert number to string
+// return string
+*/
+
+function numberToString(num) {
+  // Return a string of the number here!
+}
+
+// Test case
+const test1 = numberToString(67) // '67'
+
+console.log(test1);
---
+++ 8kyu/convert_a_number_to_a_string!/readme.md
@@ -0,0 +1,18 @@
+
+# Convert a Number to a String!
+
+View [this kata](https://www.codewars.com/kata/5265326f5fda8eb1160004c8/) on [Codewars](https://www.codewars.com)
+
+## Instructions
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Example:
+
+```text
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+```
