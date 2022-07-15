022486c682bf1443d8ea3a6f042bfdbc154d0753
Tobi Balogun <tobi.balo@gmail.com>
2022-07-15 10:05:23 +0100

    Add test cases to "Highest Scoring Word" kata

--- 6kyu/highest_scoring_word/app.js
+++ 6kyu/highest_scoring_word/app.js
@@ -1,3 +1,13 @@
 function high(x){
 
 }
+
+// Test cases
+console.log(high('man i need a taxi up to ubud')) // 'taxi'
+console.log(high('what time are we climbing up the volcano')) // 'volcano'
+console.log(high('take me to semynak')) // 'semynak'))
+console.log(high('aa b')) // 'aa'
+console.log(high('b aa')) // 'b'
+console.log(high('bb d')) // 'bb'
+console.log(high('d bb')) // 'd'
+console.log(high('aaa b')) // 'aaa'
