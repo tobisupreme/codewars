e86e12fb36188373e21ae4a364a3e18e895f2e01
Tobi Balogun <tobi.balo@gmail.com>
2022-07-20 04:37:01 +0100

    Initialise "Sum of positive" kata

---
+++ 8kyu/sum_of_positive/app.js
@@ -0,0 +1,14 @@
+function positiveSum(arr) {}
+
+// Test cases
+const test1 = positiveSum([1, 2, 3, 4, 5]); // 15
+const test2 = positiveSum([1, -2, 3, 4, 5]); // 13
+const test3 = positiveSum([]); // 0
+const test4 = positiveSum([-1, -2, -3, -4, -5]); // 0
+const test5 = positiveSum([-1, 2, 3, 4, -5]); // 9
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
