d6e36317e873335ccb602a340da428aa0e46ea54
Tobi Balogun <tobi.balo@gmail.com>
2022-07-22 03:03:59 +0100

    Initialise "Reverse Words" kata

---
+++ 7kyu/reverse_words/app.js
@@ -0,0 +1,14 @@
+function reverseWords(str) {
+  // Go for it
+}
+
+// Test cases
+const test1 = descendingOrder('The quick brown fox jumps over the lazy dog.') // 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+const test2 = descendingOrder('apple') // 'elppa');
+const test3 = descendingOrder('a b c d') // 'a b c d');
+const test4 = descendingOrder('double  spaced  words') // 'elbuod  decaps  sdrow');
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
---
+++ 7kyu/reverse_words/readme.md
@@ -0,0 +1,14 @@
+# Reverse Words
+
+View [this kata](https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/) on [Codewars](https://www.codewars.com)
+
+## Instructions
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples:
+
+```text
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+```
