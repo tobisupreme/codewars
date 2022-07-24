0d1ddb6c55db95908828f26e992ea594c443c51e
Tobi Balogun <tobi.balo@gmail.com>
2022-07-24 04:23:28 +0100

    Initialise "Sum of two lowest positive integers" kata

---
+++ 7kyu/sum_of_two_lowest_positive_integers/app.js
@@ -0,0 +1,16 @@
+function sumTwoSmallestNumbers(numbers) {
+  //Code here
+}
+
+// Test cases
+const test1 = sumTwoSmallestNumbers([5, 8, 12, 19, 22]); // 13 , "Sum should be 13"
+const test2 = sumTwoSmallestNumbers([15, 28, 4, 2, 43]); // 6 , "Sum should be 6"
+const test3 = sumTwoSmallestNumbers([3, 87, 45, 12, 7]); // 10 , "Sum should be 10"
+const test4 = sumTwoSmallestNumbers([23, 71, 33, 82, 1]); // 24 , "Sum should be 24"
+const test5 = sumTwoSmallestNumbers([52, 76, 14, 12, 4]); // 16 , "Sum should be 16"
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
