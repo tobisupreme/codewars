872f8e388a34fefab32558736f461f2f87ec1bfa
Tobi Balogun <tobi.balo@gmail.com>
2022-07-30 03:42:07 +0100

    Initialise "Sum Array" kata

---
+++ 8kyu/sum_array/app.js
@@ -0,0 +1,12 @@
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+    
+};
+
+// Test cases
+const test1 = sum([]) // 0
+const test2 = sum([1, 5.2, 4, 0, -1]) // 9.2
+
+console.log(test1);
+console.log(test2);
