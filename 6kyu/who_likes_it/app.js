044e467d4ec3bc31fbe562279b47e5e9d4dbd95b
Tobi Balogun <tobi.balo@gmail.com>
2022-08-01 19:05:34 +0100

    Add pseudocode to "Who likes it?" kata

--- 6kyu/who_likes_it/app.js
+++ 6kyu/who_likes_it/app.js
@@ -1,3 +1,33 @@
+/*
+PREP:
+Parameters:
+This function takes in an array
+
+Returns:
+This function returns a string
+
+Examples:
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Pseudocode:
+// Function takes in an array
+// Count elements in the array
+// IF array has 0 elements
+  // return "no one likes this"
+ELSE IF array has 1 element
+  // return `${array[0]} likes this`
+ELSE IF array has 2 elements
+  // return `${array[0]} and ${array[1]} like this`
+ELSE IF array has 3 elements
+  //  return `${array[0]}, ${array[1]} and ${array[2]} like this`
+ELSE
+  // return `${array[0]}, ${array[1]} and ${array.length - 2} others like this`
+*/
+
 function likes(names) {
   // TODO
 }
