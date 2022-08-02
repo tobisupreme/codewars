f86a468b28f60131756fe34e95c4fbe3ad04ce49
Tobi Balogun <tobi.balo@gmail.com>
2022-08-02 15:59:33 +0100

    Add test cases to "Descending Order" kata

--- 7kyu/descending_order/app.js
+++ 7kyu/descending_order/app.js
@@ -1,3 +1,12 @@
+
 function descendingOrder(n){
   //...
 }
+
+// Test cases
+descendingOrder(0) // 0
+descendingOrder(1) // 1
+descendingOrder(111) // 111
+descendingOrder(15) // 51
+descendingOrder(1021) // 2110
+descendingOrder(123456789) // 987654321
