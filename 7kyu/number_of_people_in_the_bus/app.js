9adce2eb96691427f70c208f71d29e031235fcc5
Tobi Balogun <tobi.balo@gmail.com>
2022-08-02 16:57:17 +0100

    Add PREP to "Number of People in the Bus" kata

--- 7kyu/number_of_people_in_the_bus/app.js
+++ 7kyu/number_of_people_in_the_bus/app.js
@@ -1,3 +1,24 @@
+/* 
+PREP:
+Parameters:
+This function takes in an array
+
+Returns:
+This function returns a number
+
+Examples:
+Input of [[10,0],[3,5],[5,8]]) returns 5
+Input of [[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]) returns 17
+Input of [[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]) returns 21
+Input of [[0,0]]) returns 0
+
+Pseudocode:
+// Function takes in an array with two elements
+// Sum all the first elements in the input array
+// Sum all the second elements in the input array
+// Return the difference between the sum of all the first elements and the sum of all the second elements in the array
+*/
+
 var number = function(busStops) {
   // Good Luck!
 }
