ca63488e723d850064754589dd70a907f36b8b09
Tobi Balogun <tobi.balo@gmail.com>
2022-08-02 16:42:43 +0100

    Initial commit - "Number of People in the Bus" kata

---
+++ 7kyu/number_of_people_in_the_bus/app.js
@@ -0,0 +1,14 @@
+var number = function(busStops) {
+  // Good Luck!
+}
+
+// Test cases
+const test1 = number([[10,0],[3,5],[5,8]]); // 5
+const test2 = number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]); // 17
+const test3 = number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]); // 21
+const test4 = number([[0,0]]); // 0
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
