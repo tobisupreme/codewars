753e506dd4a41a9b7ac170486cb0dc03c227be53
Tobi Balogun <tobi.balo@gmail.com>
2022-08-22 04:09:51 +0100

    Initialise "Delete occurrences of an element if it occurs more than n times" kata

---
+++ 6kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/app.js
@@ -0,0 +1,10 @@
+function deleteNth(arr, n) {
+    // ...
+}
+
+// Test cases
+const test1 = deleteNth([20, 37, 20, 21], 1); // [20,37,21]
+const test2 = deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3); // [1, 1, 3, 3, 7, 2, 2, 2]
+
+console.log(test1);
+console.log(test2);
