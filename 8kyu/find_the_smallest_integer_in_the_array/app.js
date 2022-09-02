d5dabc350c0fd3981ade0e83e7cd9bd51ba86b70
Tobi Balogun <tobi.balo@gmail.com>
2022-09-02 21:36:08 +0100

    Complete "Find the smallest integer in the array" kata

--- 8kyu/find_the_smallest_integer_in_the_array/app.js
+++ 8kyu/find_the_smallest_integer_in_the_array/app.js
@@ -20,7 +20,13 @@ Pseudocode:
 */
 
 function findSmallestInt(args) {
-
+  let min = args[0]
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < min) {
+      min = args[i]
+    }
+  }
+  return min
 }
 
 // Test cases
