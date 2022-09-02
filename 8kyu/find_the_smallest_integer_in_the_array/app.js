a341702a9443ad1ed709ebba17e147a9b060e5de
Tobi Balogun <tobi.balo@gmail.com>
2022-09-02 21:35:06 +0100

    Update function and test cases

--- 8kyu/find_the_smallest_integer_in_the_array/app.js
+++ 8kyu/find_the_smallest_integer_in_the_array/app.js
@@ -1,13 +1,16 @@
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-  
+  function findSmallestInt(args) {
+
   }
-}
 
 // Test cases
-const test1 = sif.findSmallestInt([78,56,232,12,8]) // 8,'Should return the smallest int 8'
-const test2 = sif.findSmallestInt([78,56,232,12,18]) // 12,'Should return the smallest int 12'
-const test3 = sif.findSmallestInt([78,56,232,412,228]) // 56,'Should return the smallest int 56'
-const test4 = sif.findSmallestInt([78,56,232,12,0]) // 0,'Should return the smallest int 0'
-const test5 = sif.findSmallestInt([1,56,232,12,8]) // 1,'Should return the smallest int 1'
+const test1 = findSmallestInt([78,56,232,12,8]) // 8,'Should return the smallest int 8'
+const test2 = findSmallestInt([78,56,232,12,18]) // 12,'Should return the smallest int 12'
+const test3 = findSmallestInt([78,56,232,412,228]) // 56,'Should return the smallest int 56'
+const test4 = findSmallestInt([78,56,232,12,0]) // 0,'Should return the smallest int 0'
+const test5 = findSmallestInt([1,56,232,12,8]) // 1,'Should return the smallest int 1'
   
+console.log(test1)
+console.log(test2)
+console.log(test3)
+console.log(test4)
+console.log(test5)
