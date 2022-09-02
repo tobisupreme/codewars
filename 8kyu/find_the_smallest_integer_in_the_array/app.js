ddfd6dd7a4a13396f24e14545ab27fe5f30b2c30
Tobi Balogun <tobi.balo@gmail.com>
2022-09-02 21:35:44 +0100

    Add PREP to "Find the smallest integer in the array" kata

--- 8kyu/find_the_smallest_integer_in_the_array/app.js
+++ 8kyu/find_the_smallest_integer_in_the_array/app.js
@@ -1,6 +1,27 @@
-  function findSmallestInt(args) {
+/*
+PREP:
+Parameters: This function takes in an array of numbers
 
-  }
+Returns: This function returns a number
+
+Examples: 
+See test cases below
+
+Pseudocode:
+- Function takes in an array
+- Declare a variable min 
+- Loop through array
+- Assign first element of array to min
+- compare current element to min
+- IF current element is less than min
+  - assign current element to min
+- End If
+- return min
+*/
+
+function findSmallestInt(args) {
+
+}
 
 // Test cases
 const test1 = findSmallestInt([78,56,232,12,8]) // 8,'Should return the smallest int 8'
