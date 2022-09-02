e329e33fa2600054250c72532eafbb606c15ab9e
Tobi Balogun <tobi.balo@gmail.com>
2022-09-02 21:13:29 +0100

    Add test cases to "Find the smallest integer in the array" kata

--- 8kyu/find_the_smallest_integer_in_the_array/app.js
+++ 8kyu/find_the_smallest_integer_in_the_array/app.js
@@ -3,3 +3,11 @@ class SmallestIntegerFinder {
   
   }
 }
+
+// Test cases
+const test1 = sif.findSmallestInt([78,56,232,12,8]) // 8,'Should return the smallest int 8'
+const test2 = sif.findSmallestInt([78,56,232,12,18]) // 12,'Should return the smallest int 12'
+const test3 = sif.findSmallestInt([78,56,232,412,228]) // 56,'Should return the smallest int 56'
+const test4 = sif.findSmallestInt([78,56,232,12,0]) // 0,'Should return the smallest int 0'
+const test5 = sif.findSmallestInt([1,56,232,12,8]) // 1,'Should return the smallest int 1'
+  
