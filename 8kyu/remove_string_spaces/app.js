ba8679b1f0eb9e08c00e63333328d9f0ae35df87
Tobi Balogun <tobi.balo@gmail.com>
2022-10-04 09:47:19 +0100

    move: "Remove String Spaces" kata

---
+++ 8kyu/remove_string_spaces/app.js
@@ -0,0 +1,12 @@
+function noSpace(x) {
+  // Split string into array of individual characters
+  let str = x.split('')
+  // compare each character for space
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      str.splice(i, 1) // delete if character is space
+      i-- // adjust for deleted element by taking the index back a step
+    } else continue
+  }
+  return str.join('')
+}
---
+++ 8kyu/remove_string_spaces/readme.md
@@ -0,0 +1,7 @@
+# Remove String Spaces
+
+View [this kata](https://www.codewars.com/kata/57eae20f5500ad98e50002c5/) on [Codewars](https://www.codewars.com)
+
+## Instructions
+
+Simple, remove the spaces from the string, then return the resultant string.
