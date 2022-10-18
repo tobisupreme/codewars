4ba0dfd67a8e82a00fccf5e33f3156973f1cb55d
Tobi Balogun <tobi.balo@gmail.com>
2022-10-18 04:58:55 +0100

    Add test cases to - "Reversed Strings" kata

--- 8kyu/reversed_strings/app.js
+++ 8kyu/reversed_strings/app.js
@@ -1,1 +1,7 @@
 function solution(str) {}
+
+// Test cases
+console.log(solution('world')) // 'dlrow'
+console.log(solution('hello')) // 'olleh'
+console.log(solution('')) // ''
+console.log(solution('h')) // 'h'
